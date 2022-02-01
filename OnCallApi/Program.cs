05e6120353616c4a614abaf7703cba33f0a3488b
SwapnaRamasamy <kr.swapna1@gmail.com>
2022-02-01 09:43:30 -0500

    Add project files.

---
+++ OnCallApi/Program.cs
@@ -0,0 +1,39 @@
+
+using System.Net;
+using System.Text.Json;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/", async (IWebHostEnvironment env) =>
+{
+    var file = Path.Combine(env.ContentRootPath, "Data", "schedule.json");
+
+    var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+
+    using var reader = new StreamReader(fileStream);
+    var response = await JsonSerializer.DeserializeAsync<Dictionary<string, OnCallEntry>>(reader.BaseStream);
+    fileStream.Close();
+
+    var addy = Dns.GetHostName();
+    app.Logger.LogInformation($"Reading Data from {file} at {addy}");
+    return Results.Ok(response);
+});
+
+app.Run();
+
+public record OnCallEntry(string name, string phone);
